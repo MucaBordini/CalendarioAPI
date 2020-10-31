6b28195ff184559afb4122aab2dbd846865e71cb
MucaBordini <mucabordini@gmail.com>
2020-10-31 14:51:47 -0300

    Corrigindo mensagens e acrescentando campos no Evento

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-//import routes from './routes';
+import routes from './routes';
 
 const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustercalendario.gy82l.mongodb.net/<dbname>?retryWrites=true&w=majority`
 
@@ -27,7 +27,7 @@ mongoose.connection.on('connected', () => {
     console.log('Mongoose connected');
 });
 
-//app.use(routes);
+app.use(routes);
 
 app.listen(8000);
 console.log('Server is running at http://localhost:8000');
